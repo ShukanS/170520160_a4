47389e37c6a9ff5524fdd52cbcc76f2e47eee594
shukan22 <shukan.shah22@gmail.com>
2019-12-09 20:12:47 -0500

    First commit

---
+++ q1/www/js/app.js
@@ -0,0 +1,13 @@
+function on(imgURL) {
+      document.getElementById("myImage").src = imgURL;
+      document.getElementById("overlay").style.display = "block";
+    }
+
+    function off() {
+      document.getElementById("overlay").style.display = "none";
+    }
+
+    function on1(imgURL) {
+          document.getElementById("myImage2").src = imgURL;
+          document.getElementById("overlay").style.display = "block";
+        }
